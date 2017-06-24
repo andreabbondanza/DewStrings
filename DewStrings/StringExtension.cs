dfc9960e143163816931defbfa4904bfb53eb721
Andrea Vincenzo Abbondanza <andreabbondanza.developer@outlook.com>
2017-06-25 00:52:41 +0200

    Concat functions and example

--- DewStrings/StringExtension.cs
+++ DewStrings/StringExtension.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace DewExtensions
@@ -26,7 +25,6 @@ namespace DewExtensions
                 return Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
             return false;
         }
-
         /// <summary>
         /// Capitalize all words in a string
         /// </summary>
@@ -36,11 +34,71 @@ namespace DewExtensions
         {
             var words = s.Split(' ');
             var result = string.Empty;
-            foreach (var item in words)
+            return result.ConcatWithChar(words,' ');
+        }
+        /// <summary>
+        /// Check if a string is empty or null
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return s == null || s == string.Empty;
+        }
+        /// <summary>
+        /// Concat the current string with an array of string with selected character 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="concat"></param>
+        /// <param name="strings"></param>
+        /// <returns></returns>
+        public static string ConcatWithChar(this string s, string[] strings, char concat = ' ')
+        {
+            var result = string.Empty;
+            foreach (var item in strings)
             {
-                result = result + item.Capitalize() + " ";
+                result = result.ConcatWithChar(item, concat);
             }
+            return result.RemoveFirstCharacter();
+        }
+        /// <summary>
+        /// Concat the current string with selected character
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="toConcat"></param>
+        /// <param name="concat"></param>
+        /// <returns></returns>
+        public static string ConcatWithChar(this string s, string toConcat, char concat = ' ')
+        {
+            return s + concat + toConcat;
+        }
+        /// <summary>
+        /// Remove last character from the string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string RemoveLastCharacter(this string s)
+        {
             return s.Substring(0, s.Length - 1);
+        }
+        /// <summary>
+        /// Remove first caracter from the string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string RemoveFirstCharacter(this string s)
+        {
+            return s.Substring(1, s.Length-1);
+        }
+        /// <summary>
+        /// Remove a character to a position
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static string RemoveCharacterAt(this string s, int index)
+        {
+            return s.Substring(0, index-1) + s.Substring(index, s.Length-index);
         }
     }
 }
---
+++ Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DewExtensions;
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "le persone guardano i film al cinema senza fare rumore";
+            Console.WriteLine(first);
+            Console.WriteLine("Capitalized:" + first.Capitalize());
+            Console.WriteLine("Capitalized all:" + first.CapitalizeAllWords());
+            Console.WriteLine("Is email? " + first.IsValidEmail());
+            Console.WriteLine("Concat " + first.ConcatWithChar(first.Split(' '), ','));
+            Console.WriteLine("Is null or empty? " + first.IsNullOrEmpty());
+            Console.WriteLine("Remove first: " + first.RemoveFirstCharacter());
+            Console.WriteLine("Remove last: " + first.RemoveLastCharacter());
+            Console.WriteLine("Remove at 5: " + first.RemoveCharacterAt(5));
+            Console.ReadLine();
+        }
+    }
+}
